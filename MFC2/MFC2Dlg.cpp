3524b3a75d474e971758255be9c52eb885cf3229
suntao <1090913131@qq.com>
2019-04-21 00:37:38 +0800

    MFC2第一次提提交

---
+++ MFC2/MFC2Dlg.cpp
@@ -0,0 +1,146 @@
+﻿
+// MFC2Dlg.cpp: 实现文件
+//
+
+#include "stdafx.h"
+#include "MFC2.h"
+#include "MFC2Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFC2Dlg 对话框
+//int m_Listid;
+
+
+CMFC2Dlg::CMFC2Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFC2_DIALOG, pParent)
+	, m_Name(_T(""))
+	, m_Sid(_T(""))
+	, m_Value(_T(""))
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFC2Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_LIST1, m_ListCon);
+	DDX_Text(pDX, NAME, m_Name);
+	DDX_Text(pDX, UID, m_Sid);
+	DDX_Text(pDX, SCORE, m_Value);
+}
+
+BEGIN_MESSAGE_MAP(CMFC2Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDOK, &CMFC2Dlg::OnBnClickedOk)
+	ON_BN_CLICKED(IDCANCEL, &CMFC2Dlg::OnBnClickedCancel)
+	ON_LBN_SELCHANGE(IDC_LIST1, &CMFC2Dlg::SELCHANGE)
+	ON_BN_CLICKED(DEL, &CMFC2Dlg::OnBnClickedDel)
+	ON_BN_CLICKED(SHOW, &CMFC2Dlg::OnBnClickedShow)
+END_MESSAGE_MAP()
+
+
+// CMFC2Dlg 消息处理程序
+
+BOOL CMFC2Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMFC2Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMFC2Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFC2Dlg::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	//CDialogEx::OnOK();
+	CString name("姓名：");
+	CString sid("学号：");
+	CString score("成绩：");
+	UpdateData();
+	CString str = name + this->m_Name + sid + this->m_Sid + score + this->m_Value;
+	this->m_ListCon.AddString(str);
+}
+
+
+void CMFC2Dlg::OnBnClickedCancel()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnCancel();
+}
+
+
+void CMFC2Dlg::SELCHANGE()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	m_Listid = this->m_ListCon.GetCurSel();
+}
+
+
+void CMFC2Dlg::OnBnClickedDel()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	this->m_ListCon.DeleteString(this->m_Listid);
+}
+
+
+void CMFC2Dlg::OnBnClickedShow()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CString str;
+	CString score("成绩");
+	this->m_ListCon.GetText(this->m_Listid, str);
+	int x = str.Find(score, 0);
+	int length = str.GetLength();
+	str = str.Right(length - x);
+	MessageBox(str);
+}
---
+++ MFC2/MFC2Dlg.h
@@ -0,0 +1,43 @@
+﻿
+// MFC2Dlg.h: 头文件
+//
+
+#pragma once
+
+
+// CMFC2Dlg 对话框
+class CMFC2Dlg : public CDialogEx
+{
+// 构造
+public:
+	CMFC2Dlg(CWnd* pParent = nullptr);	// 标准构造函数
+	int m_Listid;
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFC2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+	afx_msg void OnBnClickedCancel();
+	CListBox m_ListCon;
+	afx_msg void SELCHANGE();
+	afx_msg void OnBnClickedDel();
+	CString m_Name;
+	CString m_Sid;
+	CString m_Value;
+	afx_msg void OnBnClickedShow();
+};
